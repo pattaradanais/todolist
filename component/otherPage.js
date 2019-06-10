cc6f052f24befe0853ed833bb2510ed32dffa1ee
DAI <dpatt_s@hotmail.com>
2019-06-10 14:34:32 +0700

    card finish

---
+++ component/otherPage.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+
+export default class otherPage extends Component{
+
+    render(){
+        return(
+            <View>This is other page!!!</View>
+        );
+    }
+}
